14fd6c26b2b84166f7c6d03edaad4debbf9bb5e7
andreirotaru1999 <andreirotaru1999@yahoo.com>
2022-08-21 12:56:46 +0300

    added image for article

--- blog-bk/article/article.controller.js
+++ blog-bk/article/article.controller.js
@@ -3,14 +3,36 @@ const router = express.Router()
 const service = require("./article.service.js")
 const middleware = require('../middleware');
 const jwt_decode = require('jwt-decode');
+//const multer  = require('multer')
+//const upload = multer({ dest: 'uploads/' })
+var path = require('path');
+var multer = require('multer');
 
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '.jpg') //Appending .jpg
+  }
+})
 
-router.post('/new', middleware.authenticationGuard, middleware.roleGuard("admin"), async (req,res) => {
+var upload = multer({ storage: storage });
+
+router.post('/new', middleware.authenticationGuard, middleware.roleGuard("admin"), upload.single('image'), async (req,res) => {
+    console.log(req.cookies);
     let cookie = req.cookies['jwt'];
     cookie = jwt_decode(cookie);
-    await service.createArticle(req.body,cookie.user.username);
+    if(req.file) {
+        const imagePath = req.file.filename;
+        await service.createArticle(req.body,cookie.user.username, imagePath);
+    } else {
+        await service.createArticle(req.body,cookie.user.username);
+    }
     res.json(req.body);
 })
+
+
 router.get('', async (req,res) => {
     articles = await service.getArticles();
     res.json(articles);
@@ -21,10 +43,19 @@ router.get('/:id', async (req,res) => {
     res.json(articles);
 })
 
-router.put('/:id/edit', middleware.authenticationGuard, middleware.roleGuard("admin"), async (req,res) => {
+router.get('/image/:imageId', async (req, res) => {
+    imagePath = '/uploads/' + req.params.imageId;
+    res.sendFile(imagePath, { root: path.join(__dirname, '../') });
+})
+
+router.put('/:id/edit', middleware.authenticationGuard, middleware.roleGuard("admin"), upload.single('image'), async (req,res) => {
+    if(req.file) {
+        const imagePath = req.file.filename;
+        await service.editArticle(req.body, req.params.id, imagePath);
+    } else {
+        await service.editArticle(req.body, req.params.id);
+    }
     res.json(req.body)
-    service.editArticle(req.body, req.params.id);
-   
 })
 
 router.delete('/:id', async (req,res) => {
--- blog-bk/article/article.service.js
+++ blog-bk/article/article.service.js
@@ -16,28 +16,27 @@ async function findArticleById(id) {
     return articles.rows[0];
 }
 
-async function createArticle(article, username) {
+async function createArticle(article, username, imagePath) {
     const user = await authService.findUserByUsername(username);
     console.log("article:",article);
-    saveImage(article.image);
     db.query('INSERT INTO article(title, created_on, updated_on, description, image, user_id) VALUES($1, $2, $3, $4, $5, $6)', [
         article.title,
         article.created_on,
         article.updated_on,
         article.description,
-        article.image,
+        imagePath,
         user.id
     ]);
 }
 
-async function editArticle(article, id) {
+async function editArticle(article, id, imagePath) {
     try {
         db.query('UPDATE article SET title = $1, created_on = $2, updated_on = $3, description = $4, image =$5 WHERE id = $6', [
             article.title,
             article.created_on,
             article.updated_on,
             article.description,
-            article.image,
+            imagePath,
             id
         ]);
     } catch (error) {
@@ -64,31 +63,31 @@ async function deleteArticle(id) {
     }
 }
 
-function saveImage(baseImage) {
-    /*path of the folder where your project is saved. (In my case i got it from config file, root path of project).*/
-    //path of folder where you want to save the image.
-    const localPath = `../images/`;
-    //Find extension of file
-    const ext = baseImage.substring(baseImage.indexOf("/")+1, baseImage.indexOf(";base64"));
-    const fileType = baseImage.substring("data:".length,baseImage.indexOf("/"));
-    //Forming regex to extract base64 data of file.
-    const regex = new RegExp(`^data:${fileType}\/${ext};base64,`, 'gi');
-    //Extract base64 data.
-    const base64Data = baseImage.replace(regex, "");
-    const rand = Math.ceil(Math.random()*1000);
-    //Random photo name with timeStamp so it will not overide previous images.
-    const filename = `Photo_${Date.now()}_${rand}.${ext}`;
+// function saveImage(baseImage) {
+//     /*path of the folder where your project is saved. (In my case i got it from config file, root path of project).*/
+//     //path of folder where you want to save the image.
+//     const localPath = `../images/`;
+//     //Find extension of file
+//     const ext = baseImage.substring(baseImage.indexOf("/")+1, baseImage.indexOf(";base64"));
+//     const fileType = baseImage.substring("data:".length,baseImage.indexOf("/"));
+//     //Forming regex to extract base64 data of file.
+//     const regex = new RegExp(`^data:${fileType}\/${ext};base64,`, 'gi');
+//     //Extract base64 data.
+//     const base64Data = baseImage.replace(regex, "");
+//     const rand = Math.ceil(Math.random()*1000);
+//     //Random photo name with timeStamp so it will not overide previous images.
+//     const filename = `Photo_${Date.now()}_${rand}.${ext}`;
     
-    //Check that if directory is present or not.
-    // if(!fs.existsSync(`${uploadPath}/uploads/`)) {
-    //     fs.mkdirSync(`${uploadPath}/uploads/`);
-    // }
-    // if (!fs.existsSync(localPath)) {
-    //     fs.mkdirSync(localPath);
-    // }
-    fs.writeFileSync(localPath+filename, base64Data, 'base64');
-    return {filename, localPath};
-}
+//     //Check that if directory is present or not.
+//     // if(!fs.existsSync(`${uploadPath}/uploads/`)) {
+//     //     fs.mkdirSync(`${uploadPath}/uploads/`);
+//     // }
+//     // if (!fs.existsSync(localPath)) {
+//     //     fs.mkdirSync(localPath);
+//     // }
+//     fs.writeFileSync(localPath+filename, base64Data, 'base64');
+//     return {filename, localPath};
+// }
 
 module.exports = {
     getArticles,
